5c628908fd374213cfada3502d1df5eef1c2f1d8
Victor Shi <victor.shi@berkeley.edu>
2023-01-09 22:47:24 -0800

    lexer completed

--- shell.py
+++ shell.py
@@ -2,7 +2,7 @@ import wir
 
 while True:
     text = input('wir > ')
-    res, err = wir.run_program(text)
+    res, err = wir.run_program('<a>', text)
 
     if err: print(err.string_form())
     else: print(res)
--- wir.py
+++ wir.py
@@ -12,6 +12,7 @@ WT_PLUS = 'PLUS'
 WT_MINUS = 'MINUS'
 
 DIGITS = '0123456789'
+SUPPORTED_CHARS = '()*/+-'
 
 class Token:
     def __init__(self, wir_type, value=None):
@@ -29,15 +30,16 @@ class Token:
 #######################################
 
 class Lexer:
-    def __init__(self, text):
+    def __init__(self, file_name, text):
         self.text = text
-        self.pos = -1
+        self.file_name = file_name
+        self.pos = LinePosition(-1, 0, -1, file_name, text)
         self.curr_char = None
         self.next_char()
     
     def next_char(self):
-        self.pos += 1
-        self.curr_char = self.text[self.pos] if self.pos < len(self.text) else None
+        self.pos.next(self.curr_char)
+        self.curr_char = self.text[self.pos.ind] if self.pos.ind < len(self.text) else None
 
     def make_tokens(self):
         tkns = []
@@ -45,31 +47,33 @@ class Lexer:
         while self.curr_char is not None:
             if self.curr_char == ' ' or self.curr_char == '\t':
                 self.next_char()
-            elif self.curr_char == '(':
-                tkns.append(Token(WT_LEFTPAR))
-                self.next_char()
-            elif self.curr_char == ')':
-                tkns.append(Token(WT_RIGHTPAR))
-                self.next_char()
-            elif self.curr_char == '*':
-                tkns.append(Token(WT_MUL))
-                self.next_char()
-            elif self.curr_char == '/':
-                tkns.append(Token(WT_DIV))
-                self.next_char()
-            elif self.curr_char == '+':
-                tkns.append(Token(WT_PLUS))
-                self.next_char()
-            elif self.curr_char == '-':
-                tkns.append(Token(WT_MINUS))
+            elif self.curr_char in SUPPORTED_CHARS:
+                curr_char_tkn = self.make_op_tkn(self.curr_char)
+                tkns.append(curr_char_tkn)
                 self.next_char()
             elif self.curr_char in DIGITS:
                 tkns.append(self.make_num_tkn())
             else:
+                start_pos = self.pos.copy_pos()
                 unknown = self.curr_char
                 self.next_char()
-                return [], IllegalCharacterError(unknown)
+                return [], IllegalCharacterError(start_pos, self.pos, unknown)
         return tkns, None
+
+    def make_op_tkn(self, curr_char):
+        if self.curr_char == '(':
+            return Token(WT_LEFTPAR)
+        elif self.curr_char == ')':
+            return Token(WT_RIGHTPAR)
+        elif self.curr_char == '*':
+            return Token(WT_MUL)
+        elif self.curr_char == '/':
+            return Token(WT_DIV)
+        elif self.curr_char == '+':
+            return Token(WT_PLUS)
+        elif self.curr_char == '-':
+            return Token(WT_MINUS)
+        return
 
     def make_num_tkn(self):
         num = ''
@@ -93,10 +97,12 @@ class Lexer:
 #######################################
 
 class LinePosition:
-    def __init__(self, ind, line_num, col_num):
+    def __init__(self, ind, line_num, col_num, file_name, file_txt):
         self.ind = ind
         self.line_num = line_num
         self.col_num = col_num
+        self.file_name = file_name
+        self.file_txt = file_txt
     
     def next(self, curr_char):
         self.ind += 1
@@ -106,13 +112,20 @@ class LinePosition:
             self.line_num += 1
             self.col_num = 0
 
+        return self
+    
+    def copy_pos(self):
+        return LinePosition(self.ind, self.line_num, self.col_num, self.file_name, self.file_txt)
+    
+
+
     
 #######################################
 # RUN HANDLER
 #######################################
 
-def run_program(text):
-    new_lexer = Lexer(text)
+def run_program(file_name, text):
+    new_lexer = Lexer(file_name, text)
     tkns, err = new_lexer.make_tokens()
     return tkns, err
 
@@ -121,13 +134,16 @@ def run_program(text):
 #######################################
 
 class Error:
-    def __init__(self, title, desc):
+    def __init__(self, start_pos, end_pos, title, desc):
+        self.start_pos = start_pos
+        self.end_pos = end_pos
         self.title = title
         self.desc = desc
     
     def string_form(self):
-        return f'{self.title}: {self.desc}'
+        return f'{self.title}: {self.desc} \n in File {self.start_pos.file_name} \n at line {self.start_pos.line_num + 1}'
 
 class IllegalCharacterError(Error):
-     def __init__(self, desc):
-         super().__init__("IllegalCharacterError", desc)
+     def __init__(self, start_pos, end_pos, desc):
+         super().__init__(start_pos, end_pos, "IllegalCharacterError", desc)
+
